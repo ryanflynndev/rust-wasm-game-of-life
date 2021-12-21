c461708d9e30391ffe4a2884581708cd965df587
Ryan Flynn <ryanflynn44@gmail.com>
2021-12-21 17:03:50 -0500

    Finished initial game of life

--- src/lib.rs
+++ src/lib.rs
@@ -87,6 +87,41 @@ impl Universe {
 
         self.cells = next;
     }
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
 }
 
 impl Display for Universe {
