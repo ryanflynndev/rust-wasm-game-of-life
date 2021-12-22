282b2b4d94f7666da792c9b976ae1ad2e3b62072
Ryan Flynn <ryanflynn44@gmail.com>
2021-12-21 22:06:37 -0500

    Made it so each cell uses on bit instead of a byte

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use std::fmt::{Display, Formatter, Result};
+use fixedbitset::FixedBitSet;
+
+extern crate js_sys;
+
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -26,7 +30,7 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 
 impl Universe {
@@ -64,42 +68,28 @@ impl Universe {
                 let cell = self.cells[index];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[index] = next_cell;
+                next.set(index, match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise
+                });
             }
         }
 
         self.cells = next;
     }
     pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
-
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let width = 256;
+        let height = 256;
+    
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+    
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.5);
+        }
 
         Universe {
             width,
@@ -115,25 +105,25 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
-    }
+    // pub fn render(&self) -> String {
+    //     self.to_string()
+    // }
 }
 
-impl Display for Universe {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
+// impl Display for Universe {
+//     fn fmt(&self, f: &mut Formatter) -> Result {
+//         for line in self.cells.as_slice().chunks(self.width as usize) {
+//             for &cell in line {
+//                 let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+//                 write!(f, "{}", symbol)?;
+//             }
+//             write!(f, "\n")?;
+//         }
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 import { Universe, Cell } from "rust-wasm-game-of-life";
 import { memory } from "rust-wasm-game-of-life/rust_wasm_game_of_life_bg";
 
-const CELL_SIZE = 10; // px
+const CELL_SIZE = 5; // px
 const GRID_COLOR = "#141414";
 const DEAD_COLOR = "#141414";
 const ALIVE_COLOR = "#57ffdc";
@@ -34,23 +34,34 @@ const drawGrid = () => {
 const getIndex = (row, column) => {
     return row * width + column;
 };
+
+const bitIsSet = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << (n % 8);
+    return (arr[byte] & mask) === mask;
+};
+
 const drawCells = () => {
     const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+    // This is updated!
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
+
     ctx.beginPath();
+
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
     
-            ctx.fillStyle = cells[idx] === Cell.Dead
-            ? DEAD_COLOR
-            : ALIVE_COLOR;
+            // This is updated!
+            ctx.fillStyle = bitIsSet(idx, cells)
+            ? ALIVE_COLOR
+            : DEAD_COLOR;
     
             ctx.fillRect(
             col * (CELL_SIZE + 1) + 1,
             row * (CELL_SIZE + 1) + 1,
-            CELL_SIZE,
-            CELL_SIZE
+                CELL_SIZE,
+                CELL_SIZE
             );
         }
     }
